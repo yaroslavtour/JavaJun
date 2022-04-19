8889033e4f2b698d252dbc7553969ddcc3f33113
yaroslavtour <tourchukov@mail.ru>
2022-04-19 13:48:59 +0300

    2 task

--- src/main/java/Pizza.java
+++ src/main/java/Pizza.java
@@ -1,10 +1,12 @@
 public class Pizza {
     public static void main(String[] args) {
-        int money = 400;
+        int money = 200;
         if (money > 500){
             System.out.println("Pizza");
         } else if (money < 100) {
             System.out.println("Doshik");
+        } else if (money > 100 & money < 300){
+            System.out.println("Гамбургер");
         } else {
             System.out.println("It's not enough for Pizza but enough for Doshik");
         }
