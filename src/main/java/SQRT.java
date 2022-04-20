9ed7c8861e33949777e7170769ed4b3dd8770ed5
yaroslavtour <tourchukov@mail.ru>
2022-04-20 14:19:28 +0300

    3 task

---
+++ src/main/java/SQRT.java
@@ -0,0 +1,8 @@
+public class SQRT {
+    public static void main(String[] args) {
+        double a = 4.567;
+        double b = 5.6789;
+        double c = Math.sqrt(a * a + b * b);
+        System.out.println("Гипотинуза равна: " + c);
+    }
+}
