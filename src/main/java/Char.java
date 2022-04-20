20313ae3c5afeb7ac3dfd7eb40a1f9f28c61ea87
yaroslavtour <tourchukov@mail.ru>
2022-04-20 14:55:22 +0300

    4 task

---
+++ src/main/java/Char.java
@@ -0,0 +1,8 @@
+public class Char {
+    public static void main(String[] args) {
+        char ch1 = 89;
+        char ch2 = 'Y';
+ //       ch1 += 1;
+        System.out.println("ch1 = " + ch1 + ", ch2 = " + ch2);
+    }
+}
