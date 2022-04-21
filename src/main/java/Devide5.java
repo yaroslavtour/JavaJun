2c05a4daa2844f595fc8886d542872eda67ea54d
yaroslavtour <tourchukov@mail.ru>
2022-04-21 19:19:19 +0300

    new task

---
+++ src/main/java/Devide5.java
@@ -0,0 +1,9 @@
+public class Devide5 {
+    public static void main(String[] args) {
+        for (int i = 100; i <= 1000; i++){
+            if(i % 2 != 0 && i % 5 == 0){
+                System.out.println(i);
+            }
+        }
+    }
+}
