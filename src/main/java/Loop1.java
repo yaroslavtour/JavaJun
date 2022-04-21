a34ea3b7a584053b1455b90aa7f6ab25994b2f11
yaroslavtour <tourchukov@mail.ru>
2022-04-21 17:47:29 +0300

    4 task

---
+++ src/main/java/Loop1.java
@@ -0,0 +1,11 @@
+public class Loop1 {
+    public static void main(String[] args) {
+        int i = 1000;
+        while (i >= 0){
+            if(i % 2 == 0){
+                System.out.println(i);
+            }
+            i--;
+        }
+    }
+}
