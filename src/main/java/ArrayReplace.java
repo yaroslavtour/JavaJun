ff6d7a5c4a122faf7826d2071ee08842946d2d57
yaroslavtour <tourchukov@mail.ru>
2022-04-28 19:48:53 +0300

    new task

---
+++ src/main/java/ArrayReplace.java
@@ -0,0 +1,15 @@
+public class ArrayReplace {
+    public static void main(String[] args) {
+        int[] first = new int[900];
+        for(int i = 0; i < first.length; i++) {
+            first[i] = i + 100;
+        };
+        int[] second = new int[first.length];
+        for(int i = 0, j = first.length - 1; i < first.length; i++, j--) {
+            second[j] = first[i];
+        };
+        for (int i : second) {
+            System.out.println(i);
+        }
+    }
+}
