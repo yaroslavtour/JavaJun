29d5325b4fa3c1de0b8cf226a9c5ab70385933b0
yaroslavtour <tourchukov@mail.ru>
2022-05-04 13:29:59 +0300

    new task

---
+++ src/main/java/SwitchCaseMonths.java
@@ -0,0 +1,45 @@
+public class SwitchCaseMonths {
+    public static void main(String[] args) {
+        int numberOfMonth = 12;
+        switch (numberOfMonth) {
+            case 1:
+                System.out.println("Январь");
+                break;
+            case 2:
+                System.out.println("Февраль");
+                break;
+            case 3:
+                System.out.println("Март");
+                break;
+            case 4:
+                System.out.println("Апрель");
+                break;
+            case 5:
+                System.out.println("Май");
+                break;
+            case 6:
+                System.out.println("Июнь");
+                break;
+            case 7:
+                System.out.println("Июль");
+                break;
+            case 8:
+                System.out.println("Август");
+                break;
+            case 9:
+                System.out.println("Сентябрь");
+                break;
+            case 10:
+                System.out.println("Октябрь");
+                break;
+            case 11:
+                System.out.println("Ноябрь");
+                break;
+            case 12:
+                System.out.println("Декабрь");
+                break;
+            default:
+                System.out.println("Такого месяца не существует.");
+        }
+    }
+}
